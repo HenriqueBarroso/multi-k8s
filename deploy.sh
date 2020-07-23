6acbf80bf2abc5f71da2a0bbd8978d12e93066ea
Henrique Barroso <henrique@personio.de>
2020-07-23 08:55:29 +0200

    added deploymeent scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t henriquebarroso/multi-client:latest -t henriquebarroso/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t henriquebarroso/multi-server:latest -t henriquebarroso/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t henriquebarroso/multi-worker:latest -t henriquebarroso/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push henriquebarroso/multi-client:latest
+docker push henriquebarroso/multi-server:latest
+docker push henriquebarroso/multi-worker:latest
+
+docker push henriquebarroso/multi-client:$SHA
+docker push henriquebarroso/multi-server:$SHA
+docker push henriquebarroso/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=henriquebarroso/multi-server:$SHA
+kubectl set image deployments/client-deployment server=henriquebarroso/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=henriquebarroso/multi-worker:$SHA
